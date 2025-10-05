0f71b1867c7ec96389b3816e108512574fbc96d9
Eljean-23 <88048873+Eljean-23@users.noreply.github.com>
2025-10-05 19:12:27 +0200

    GET /billing en GET /billing/{user} (admin)

--- python/session_calculator.py
+++ python/session_calculator.py
@@ -6,10 +6,10 @@ import uuid
 
 def calculate_price(parkinglot, sid, data):
     price = 0
-    start = datetime.strptime(data["started"], "%d-%m-%Y %H:%M:%S")
+    start = datetime.strptime(data["started"], "%Y-%m-%dT%H:%M:%SZ")
 
     if data.get("stopped"):
-        end = datetime.strptime(data["stopped"], "%d-%m-%Y %H:%M:%S")
+        end = datetime.strptime(data["stopped"], "%Y-%m-%dT%H:%M:%SZ")
     else:
         end = datetime.now()
 
