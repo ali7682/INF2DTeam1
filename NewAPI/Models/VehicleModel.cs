69fcb08556cf00b92fa021de24ea8485a8a407de
Thinh-bleh <1096055@hr.nl>
2025-10-13 14:36:14 +0200

    vehicle model en controller erin gedaan

--- NewAPI/Models/VehicleModel.cs
+++ NewAPI/Models/VehicleModel.cs
@@ -2,8 +2,8 @@ using Microsoft.VisualBasic;
 
 public class VehicleModel
 {
-    public int ID { get; set; }
-    public int UserID { get; set; }
+    public readonly int ID { get; set; }
+    public readonly int UserID { get; set; }
     public String LicensePlate{ get; set; }
 
     public String Make { get; set; }
@@ -14,7 +14,7 @@ public class VehicleModel
 
     public int Year { get; set; }
     
-    public DateTime CreatedAt { get; set; }
+    public readonly DateTime CreatedAt { get; set; }
 
     public VehicleModel(int id, int userID, String licensePlate, String make, String model, String color, int year, DateTime createdAt)
     {
