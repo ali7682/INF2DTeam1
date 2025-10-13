abf67551b004fba097a3f484d76783bfc200208b
Thinh-bleh <1096055@hr.nl>
2025-10-13 14:54:08 +0200

    null

--- NewAPI/Models/VehicleModel.cs
+++ NewAPI/Models/VehicleModel.cs
@@ -6,9 +6,9 @@ public class VehicleModel
     public int UserID { get; init; }
     public DateTime CreatedAt { get; init; }
 
-    public string LicensePlate { get; set; }
-    public string Make { get; set; }
-    public string Model { get; set; }
-    public string Color { get; set; }
+    public string? LicensePlate { get; set; }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public string? Color { get; set; }
     public int Year { get; set; }
 }
