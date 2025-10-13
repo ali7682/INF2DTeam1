7b6b1b6c41fffd4eae7ebbaeeb0ff98f2016cf0b
Davidek523 <davidov520@wp.pl>
2025-10-13 14:34:43 +0200

    Created ReservModel
    Added:
    - Reservation model

---
+++ NewAPI/Models/ReservationModel.cs
@@ -0,0 +1,25 @@
+public class ReservationModel
+{
+    public readonly int ID { get; set; }
+    public readonly int UserID { get; set; }
+    public readonly int ParkinglotID { get; set; }
+    public readonly int VehicleID { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public Enum? Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public decimal Cost { get; set; }
+
+    public ReservationModel(int id, int userID, int parkinglotID, int vehicleID, DateTime startTime, DateTime endTime, Enum status, DateTime createdAt, decimal cost)
+    {
+        ID = id;
+        UserID = userID;
+        ParkinglotID = parkinglotID;
+        VehicleID = vehicleID;
+        StartTime = startTime;
+        EndTime = endTime;
+        Status = status;
+        CreatedAt = createdAt;
+        Cost = cost;
+    }
+}
