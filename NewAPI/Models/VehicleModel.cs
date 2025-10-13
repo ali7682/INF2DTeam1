b8f47e7e32ffbf48e157c91034388e3f3c13dd43
Thinh-bleh <1096055@hr.nl>
2025-10-13 14:42:54 +0200

    constructor weggedaan

--- NewAPI/Models/VehicleModel.cs
+++ NewAPI/Models/VehicleModel.cs
@@ -16,15 +16,4 @@ public class VehicleModel
     
     public readonly DateTime CreatedAt { get; set; }
 
-    public VehicleModel(int id, int userID, String licensePlate, String make, String model, String color, int year, DateTime createdAt)
-    {
-        ID = id;
-        UserID = userID;
-        LicensePlate = licensePlate;
-        Make = make;
-        Model = model;
-        Color = color;
-        Year = year;
-        CreatedAt = createdAt;
-    }
 }
