6652abe2590544e2f1860b1de9fea55804c7c114
ali7682 <alialimail2003@gmail.com>
2025-10-15 12:07:36 +0200

    Minor fixes
    Enum naar string in reservation model, naam changes van files

--- NewAPI/Models/ParkinglotModel.cs
+++ NewAPI/Models/ParkinglotModel.cs
@@ -1,12 +1,12 @@
-public class ParkinglotModel
+public class ParkingLotModel
 {
     public int ID { get; init; }
     public string? Name { get; set; }
     public string? Location { get; set; }
     public string? Address { get; set; }
-    public int Capacity { get; set; }
-    public int Reserved { get; set; }
-    public decimal Tariff { get; set; }
-    public decimal DayTariff { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public int? Capacity { get; set; }
+    public int? Reserved { get; set; }
+    public decimal? Tariff { get; set; }
+    public decimal? DayTariff { get; set; }
+    public DateTime? CreatedAt { get; set; }
 }
