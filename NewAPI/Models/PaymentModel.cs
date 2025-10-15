10efce7d8bbb07c1d715cd2c98bd22456940f684
Eljean-23 <88048873+Eljean-23@users.noreply.github.com>
2025-10-15 12:15:57 +0200

    Payment controller

--- NewAPI/Models/PaymentModel.cs
+++ NewAPI/Models/PaymentModel.cs
@@ -1,7 +1,7 @@
 public sealed class PaymentModel
 {
     public int TransactionId { get; init; }
-    public double? Amount { get; set; }
+    public decimal? Amount { get; set; }
     public string? Initiator { get; set; }
     public DateTime? Created_at { get; set; }
     public DateTime? Completed { get; set; }
