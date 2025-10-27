3fff8334b8bd950bbc472f52a367aeb179510c8f
Eljean-23 <88048873+Eljean-23@users.noreply.github.com>
2025-10-27 13:51:57 +0100

    Payment controller

--- NewAPI/Controllers/PaymentController.cs
+++ NewAPI/Controllers/PaymentController.cs
@@ -17,12 +17,56 @@ namespace NewAPI.Controllers
         [HttpPost("Payments")]
         public async Task<ActionResult<PaymentModel>> Payments([FromBody] PaymentRequest body, CancellationToken ct)
         {
+
+            string sessionToken = HttpContext.Request.Headers.Authorization.ToString();
+            UserModel? user = SessionManager.GetSession(sessionToken);
+
+            if (sessionToken == null || user == null)
+            {
+                return Unauthorized("Unauthorized: Invalid or missing session token");
+            }
+
             if (body is null || string.IsNullOrWhiteSpace(body.Transaction) || body.Amount <= 0)
                 return BadRequest(new { error = "Bad request: Missing or invalid payment details" });
 
             PaymentModel newPayment = new()
             {
                 Amount = body.Amount,
+                Initiator = body.Transaction,
+                Created_at = DateTime.UtcNow,
+                Completed = null,
+                Hash = Guid.NewGuid().ToString("N")
+            };
+
+            int newId = PaymentAccess.CreatePayment(newPayment);
+
+            return Ok(new { message = $"Payment created successfully with ID {newId}" });
+        }
+
+        [HttpPost("Payments/refund")]
+        public async Task<ActionResult<PaymentModel>> Payments([FromBody] PaymentRequest body, CancellationToken ct)
+        {
+
+            string sessionToken = HttpContext.Request.Headers.Authorization.ToString();
+            UserModel? user = SessionManager.GetSession(sessionToken);
+
+            if (sessionToken == null || user == null)
+            {
+                return Unauthorized("Unauthorized: Invalid or missing session token");
+            }
+
+            if (user.Role != "ADMIN")
+                return StatusCode(403, new { message = "Access denied" });
+
+            if (body is null || body.Amount <= 0)
+                return BadRequest(new { error = "Bad request: Missing or invalid payment details" });
+
+            if (string.IsNullOrWhiteSpace(body.Transaction))
+                body.Transaction = $"refund-{DateTime.UtcNow:yyyy-MM-dd-HH}-{Guid.NewGuid().ToString("N").Substring(0, 6)}";
+
+            PaymentModel newPayment = new()
+            {
+                Amount = -Math.Abs(body.Amount),
                 Initiator = body.Transaction,
                 Created_at = DateTime.UtcNow,
                 Completed = null,
