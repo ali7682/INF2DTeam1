b7f5956d1fa35fce9bc8d912ba1dc9aef5576bfd
Davidek523 <davidov520@wp.pl>
2025-10-28 22:45:18 +0100

    Changed name
    Changed method name

--- NewAPI/Access/PaymentDetailsAccess.cs
+++ NewAPI/Access/PaymentDetailsAccess.cs
@@ -32,7 +32,7 @@ public static class PaymentDetailsAccess
 
     // GET alle payment details
     // Endpoint: /paymentdetails
-    public static PaymentDetailsModel GetAllDetails()
+    public static PaymentDetailsModel GetAllPaymentDetails()
     {
         string cs = _config.GetConnectionString("DefaultConnection")!;
         using MySqlConnection conn = new(cs);
