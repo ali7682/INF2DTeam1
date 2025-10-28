fdea50e94501df083b8cb399d31568596dcb9893
Davidek523 <davidov520@wp.pl>
2025-10-28 22:25:27 +0100

    Fixed endpoint
    Fixed:
    - endpoint Get all payments and added get all payments method in access

--- NewAPI/Access/PaymentAccess.cs
+++ NewAPI/Access/PaymentAccess.cs
@@ -30,6 +30,29 @@ public static class PaymentAccess
         return newId;
     }
 
+    // GET alle payments
+    // Endpoint: /payments
+    public static List<PaymentModel> GetAllPayments()
+    {
+        string cs = _config.GetConnectionString("DefaultConnection")!;
+        using MySqlConnection conn = new(cs);
+        conn.Open();
+
+        const string sql = """
+        SELECT 
+            transaction_id     AS TransactionId,
+            amount             AS Amount,
+            initiator          AS Initiator,
+            created_at         AS Created_at,
+            completed          AS Completed,
+            hash               AS Hash
+            FROM payments;
+        """;
+
+        List<PaymentModel> result = conn.Query<PaymentModel>(sql).ToList();
+        return result;
+    }
+
     public static PaymentModel GetPaymentByTransactionId(int transaction_Id)
     {
         string cs = _config.GetConnectionString("DefaultConnection")!;
