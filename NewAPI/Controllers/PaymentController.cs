dcacffe8163b979a831258a027a81007286b379e
Davidek523 <davidov520@wp.pl>
2025-10-28 23:01:43 +0100

    Transfer endpoints
    Transfered:
    - the billing endpoints from payments to payments detai;s

--- NewAPI/Controllers/PaymentController.cs
+++ NewAPI/Controllers/PaymentController.cs
@@ -131,64 +131,6 @@ namespace NewAPI.Controllers
             
             return Ok(userPayments);
         }
-
-        [HttpGet]
-        public IActionResult GetBilling()
-        {
-            string sessionToken = HttpContext.Request.Headers.Authorization.ToString();
-            UserModel? user = SessionManager.GetSession(sessionToken);
-
-            if (sessionToken == null || user == null)
-            {
-                return Unauthorized("Unauthorized: Invalid or missing session token");
-            }
-
-            List<PaymentModel> paymentDetails = PaymentDetailsAccess.GetAllPaymentDetails();
-
-            if (paymentDetails == null)
-            {
-                return NotFound("NotFound: Billing not found");
-            }
-
-            List<PaymentModel> userPaymentDetails = payment.Where(x => x.Initiator == user.Username).ToList();
-
-            return Ok(userPaymentDetails);
-        }
-
-        [HttpGet("{userName}")]
-        public IActionResult GetBillingByUser(string userName)
-        {
-            string sessionToken = HttpContext.Request.Headers.Authorization.ToString();
-            UserModel? user = SessionManager.GetSession(sessionToken);
-
-            if (sessionToken == null || user == null)
-            {
-                return Unauthorized("Unauthorized: Invalid or missing session token");
-            }
-
-            if (!user.IsAdmin())
-            {
-                return StatusCode(403, new { message = "Forbidden: you do not have access to biling" });
-            }
-
-            UserModel? requestUser = UserAccess.GetUserByUsername(userName);
-
-            if (requestUser == null)
-            {
-                return NotFound("NotFound: User not found");
-            }
-
-            List<PaymentModel> paymentDetails = PaymentDetailsAccess.GetAllPaymentDetails();
-
-            if (paymentDetails == null)
-            {
-                return NotFound("NotFound: Billing not found");
-            }
-
-            List<PaymentModel> userPaymentDetails = payment.Where(x => x.Initiator == requestUser.Username).ToList();
-
-            return Ok(userPaymentDetails);
-        }
     }
 }
     
--- NewAPI/Controllers/PaymentDetailsController.cs
+++ NewAPI/Controllers/PaymentDetailsController.cs
@@ -13,5 +13,65 @@ namespace NewAPI.Controllers
             _config = config;
             PaymentDetailsAccess.SetConfig(_config);
         }
+
+        // GET /billings
+        [HttpGet]
+        public IActionResult GetBilling()
+        {
+            string sessionToken = HttpContext.Request.Headers.Authorization.ToString();
+            UserModel? user = SessionManager.GetSession(sessionToken);
+
+            if (sessionToken == null || user == null)
+            {
+                return Unauthorized("Unauthorized: Invalid or missing session token");
+            }
+
+            List<PaymentModel> paymentDetails = PaymentDetailsAccess.GetAllPaymentDetails();
+
+            if (paymentDetails == null)
+            {
+                return NotFound("NotFound: Billing not found");
+            }
+
+            List<PaymentModel> userPaymentDetails = payment.Where(x => x.Initiator == user.Username).ToList();
+
+            return Ok(userPaymentDetails);
+        }
+
+        // GET /billings/{username}
+        [HttpGet("{userName}")]
+        public IActionResult GetBillingByUser(string userName)
+        {
+            string sessionToken = HttpContext.Request.Headers.Authorization.ToString();
+            UserModel? user = SessionManager.GetSession(sessionToken);
+
+            if (sessionToken == null || user == null)
+            {
+                return Unauthorized("Unauthorized: Invalid or missing session token");
+            }
+
+            if (!user.IsAdmin())
+            {
+                return StatusCode(403, new { message = "Forbidden: you do not have access to biling" });
+            }
+
+            UserModel? requestUser = UserAccess.GetUserByUsername(userName);
+
+            if (requestUser == null)
+            {
+                return NotFound("NotFound: User not found");
+            }
+
+            List<PaymentModel> paymentDetails = PaymentDetailsAccess.GetAllPaymentDetails();
+
+            if (paymentDetails == null)
+            {
+                return NotFound("NotFound: Billing not found");
+            }
+
+            List<PaymentModel> userPaymentDetails = payment.Where(x => x.Initiator == requestUser.Username).ToList();
+
+            return Ok(userPaymentDetails);
+        }
     }
 }
