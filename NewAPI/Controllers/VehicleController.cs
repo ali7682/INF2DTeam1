47fc5a21e5e51cb9a0f06507bc6697225a06861f
Thinh-bleh <1096055@hr.nl>
2025-10-29 09:58:27 +0100

    userid mee geven aan vehicle

--- NewAPI/Controllers/VehicleController.cs
+++ NewAPI/Controllers/VehicleController.cs
@@ -269,7 +269,8 @@ public class VehiclesController : ControllerBase
             Model = newVehicle.Model,
             Color = newVehicle.Color,
             Year = newVehicle.Year,
-            CreatedAt = DateTime.Now
+            CreatedAt = DateTime.Now,
+            UserID = user.Id
         };
 
         int newVehicleId = VehicleAccess.CreateVehicle(vehicleToCreate);
@@ -287,7 +288,8 @@ public class VehiclesController : ControllerBase
             Model = newVehicle.Model,
             Color = newVehicle.Color,
             Year = newVehicle.Year,
-            CreatedAt = DateTime.Now
+            CreatedAt = DateTime.Now,
+            UserID = user.Id
         };
 
         return Ok(new
