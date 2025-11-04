ec136b7f06010d3e2672193e774bdf894a3478d2
Davidek523 <davidov520@wp.pl>
2025-11-04 13:53:35 +0100

    Added a unit test
    Added:
    - a unittest for payment

---
+++ NewAPITests/PaymentsTest.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewAPI.Controllers;
+using NewAPI.Models;
+
+namespace NewAPITests
+{
+    [TestClass]
+    public class PaymentsController
+    {
+        [TestMethod]
+        public void TestGetPayments()
+        {
+            var controller = new PaymentsController();
+            
+        }
+    }
+}
