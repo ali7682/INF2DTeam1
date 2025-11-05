a549914bd6a382717e69661a1b3c564f717066e6
Davidek523 <davidov520@wp.pl>
2025-11-05 10:33:08 +0100

    Added tests
    Added:
    - tests for /payments en /payments/username

--- NewAPITests/PaymentsTest.cs
+++ NewAPITests/PaymentsTest.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NewAPI.Controllers;
 using NewAPI.Models;
@@ -5,13 +7,38 @@ using NewAPI.Models;
 namespace NewAPITests
 {
     [TestClass]
-    public class PaymentsController
+    public class PaymentsControllerTests
     {
+        // Test GET: /payments
         [TestMethod]
         public void TestGetPayments()
         {
-            var controller = new PaymentsController();
-            
+            PaymentsController controller = new PaymentsController();
+            var result = controller.GetPayments() as OkObjectResult;
+
+            Assert.IsNotNull(result, "Expected a OK object but got null");
+            Assert.AreEqual(200, result.StatusCode, "Expected 200 OK response");
+        }
+
+        // Test GET: /payments/{username}
+        [TestMethod]
+        public void TestValidGetPaymentsByUserName()
+        {
+            PaymentsController controller = new PaymentsController();
+            var result = controller.GetPaymentsByUserName("JohnDeer12") as OkObjectResult;
+
+            Assert.IsNotNull(result, "Expected a OK object but got null");
+            Assert.AreEqual(200, result.StatusCode, "Expected 200 OK response");
+        }
+
+        [TestMethod]
+        public void TestInvalidGetPaymentsByUserName()
+        {
+            PaymentsController controller = new PaymentsController();
+            var result = controller.GetPaymentsByUserName("") as BadRequestObjectResult;
+
+            Assert.IsNotNull(result, "Expected a Bad Request object but got null");
+            Assert.AreEqual(400, result.StatusCode, "Expected 400 Bad Request response");
         }
     }
 }
