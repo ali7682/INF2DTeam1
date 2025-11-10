491b5a7c6ea59253c1ca256cc79efacf20be50ba
melshr <183404402+melshr@users.noreply.github.com>
2025-11-10 13:20:48 +0100

    xUnit

---
+++ NewAPITests/EndpointTests/AuthControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace NewAPITests.EndpointTests
+{
+    class AuthControllerTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public AuthControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory.WithWebHostBuilder(_ => { });
+        }
+    }
+}
