49d9bd98da393d494640649c03770a66aae5865a
melshr <183404402+melshr@users.noreply.github.com>
2025-11-11 15:49:50 +0100

    Admin exception die niet in Py codebase bestond toegevoegd

--- NewAPI/Controllers/VehicleController.cs
+++ NewAPI/Controllers/VehicleController.cs
@@ -22,7 +22,7 @@ public class VehiclesController : ControllerBase
             return NotFound("NotFound: Vehicle not found");
         }
 
-        if (!(user.Id == vehicle.UserID))
+        if (user.Role != "ADMIN" && user.Id != vehicle.UserID)
         {
             //return Forbid("Forbidden: You do not have access to this vehicle");
             return StatusCode(403, new { message = "Forbidden: You do not have access to this vehicle" });
