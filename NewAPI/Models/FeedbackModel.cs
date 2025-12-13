c17dc3ce297ce7090c274b4a5af096060fe9a866
melshr <183404402+melshr@users.noreply.github.com>
2025-12-13 21:24:08 +0100

    FeedbackModel toegevoegd

---
+++ NewAPI/Models/FeedbackModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NewAPI.Models
+{
+    public class FeedbackModel
+    {
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public int ParkingSessionId { get; set; }
+    }
+}
